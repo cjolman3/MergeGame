aa3798a1c9d081244ba28c0d16f67a3e5df6ec74
cjolman3 <charlie.jolman@gmail.com>
2022-05-18 22:36:53 -0400

    Added a simple laser shot
    LAser raycast from armature head, deals damage and force. Also added audio

---
+++ Assets/Scripts/RayCastLaserShoot.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RayCastLaserShoot : MonoBehaviour
+{
+    public int laserDamage = 1;
+    public float fireRate = .5f;
+    public float weaponRange = 10f;
+    public float hitForce = 100f;
+
+    public Transform head;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.5f);
+    private AudioSource laserAudio;
+    private LineRenderer laserLine;
+    private float nextFire; 
+    // Start is called before the first frame update TODO
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        laserAudio = GetComponent<AudioSource>();
+        head = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouse = Mouse.current;
+        
+        if (mouse.press.isPressed && Time.time > nextFire) 
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = head.position; //this needs to be location of eyeball or head
+            RaycastHit hit;
+            laserLine.SetPosition(0, head.position);
+            if (Physics.Raycast(rayOrigin, head.forward, out hit, weaponRange))
+            { 
+                laserLine.SetPosition (1, hit.point);
+                ShootableArmature health = hit.collider.GetComponent<ShootableArmature>();
+
+                if (health != null)
+                {
+                    health.Damage (laserDamage);
+                }
+
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce (-hit.normal * hitForce);
+                }
+            }
+            else
+            { 
+                laserLine.SetPosition(1, rayOrigin + (head.forward * weaponRange));
+            }
+        }
+        
+
+    }
+    
+    private IEnumerator ShotEffect()
+    {
+        laserAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
